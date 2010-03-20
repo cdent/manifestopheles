9cca348747d8106ab775f47e24c75212ce7a673b
Chris Dent <chris.dent@gmail.com>
2010-03-20 13:17:08 +0000

    Add the start of a renderer. This gets the basics just fine.

---
+++ test/test_render.py
@@ -0,0 +1,69 @@
+"""
+Our default renderer takes content that is
+expected to be plain text and for titles in
+a given bag, links to those titles.
+"""
+
+import shutil
+
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.model.recipe import Recipe
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.config import config
+from tiddlywebplugins.manifestopheles.contextify import render
+from tiddlywebplugins.utils import get_store
+
+def setup_module(module):
+
+    try:
+        shutil.rmtree('store')
+    except:
+        pass
+
+    store = get_store(config)
+    bag = Bag('devil-dictionary')
+    store.put(bag)
+    bag = Bag('x-manifesto')
+    store.put(bag)
+    recipe = Recipe('devil')
+    recipe.set_recipe([
+        ('devil-dictionary', ''),
+        ('x-manifesto', ''),
+        ])
+    store.put(recipe)
+    tiddler = Tiddler('fancy', 'devil-dictionary')
+    store.put(tiddler)
+    tiddler = Tiddler('house car', 'devil-dictionary')
+    store.put(tiddler)
+    tiddler = Tiddler('car', 'devil-dictionary')
+    store.put(tiddler)
+    tiddler = Tiddler('The Truth', 'x-manifesto')
+    tiddler.text = """
+I was walking and spied something fancy.
+
+Truth is, I thought it was a house car,
+but it turns out I was wrong.
+
+It was a
+      house
+         car!
+
+Which is way more fancy.
+"""
+    store.put(tiddler)
+
+
+def test_simple():
+    store = get_store(config)
+    environ = {
+            'tiddlyweb.usersign': {'name': 'devil', 'roles': []},
+            'tiddlyweb.store': store,
+            'wsgiorg.routing_args': [None, {'recipe_name': 'devil'}],
+            }
+    tiddler = store.get(Tiddler('The Truth', 'x-manifesto'))
+    output = render(tiddler, environ)
+
+    assert 'something <a title="fancy" href="/manifestos/devil/fancy">fancy</a>.' in output
+    assert 'a <a title="house car" href="/manifestos/devil/house%20car">house car</a>,' in output
+
+
---
+++ tiddlywebplugins/manifestopheles/contextify.py
@@ -0,0 +1,50 @@
+"""
+A TiddlyWeb renderer which marks up a manifesto
+and friends.
+"""
+
+# XXX tiddlyweb 1.0
+
+import re
+import urllib
+
+from tiddlyweb.model.bag import Bag
+
+def render(tiddler, environ):
+    """
+    Link found phrases to other tiddlers.
+    """
+    store = environ['tiddlyweb.store']
+    recipe_name = environ['wsgiorg.routing_args'][1]['recipe_name']
+    bag = store.get(Bag('%s-dictionary' % recipe_name))
+
+    def space_count(input):
+        return input.count(' ')
+
+    tiddler_titles = [btiddler.title for btiddler in bag.gen_tiddlers()]
+    tiddler_titles.sort(key=space_count, reverse=True)
+
+    text = tiddler.text
+
+    # build the regular expression
+    pat = []
+    for title in tiddler_titles:
+        title = re.sub(r'\s+', r'\s+', title)
+        pat.append(title)
+    pat = '|'.join(pat)
+    pat = r'\b(%s)\b' % pat
+    replace = r'<a title="\1" href="/manifestos/%s/\1">\1</a>' % recipe_name
+
+    output = re.sub(pat, replace, text)
+
+    def clean_attribute(match):
+        attr = match.group(1)
+        value = match.group(2)
+        value = re.sub('\s+', ' ', value)
+        if attr == 'href':
+            value = urllib.quote(value)
+        return '%s="%s"' % (attr, value)
+
+    output = re.sub(r'(title|href)="([^"]+)"', clean_attribute, output)
+    
+    return output
